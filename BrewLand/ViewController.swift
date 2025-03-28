fbbfb57a6cd2b3a9b7f68dae299ed73f10cfc202
Lazzat Seiilova <lseiilova@gmail.com>
2025-03-28 17:36:05 +0500

    Initial Commit

---
+++ BrewLand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrewLand
+//
+//  Created by Lazzat Seiilova on 28.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
